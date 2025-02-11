f6511edb64a1aca8a494009338e42e4cac8e7a16
Ben <benwilliamscomapi@outlook.com>
2025-02-11 13:07:27 +0000

    Add JavaScript for the faq section

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+// FAQ accordian
+
+// Adding the container makes this an event delegation instead of placing it on each header or icon etc
+
+document.addEventListener('DOMContentLoaded', function() {
+    const faqContainer = document.querySelector('.faq-content');
+
+    faqContainer.addEventListener('click', function(e){
+        const groupHeader =e.target.closest('.faq-group-header');
+        
+        if (!groupHeader) return;
+        
+        const group = groupHeader.parentElement;
+        const groupBody = group.querySelector('.faq-group-body');
+        const icon = groupHeader.querySelector('i');
+
+        //Toggle icon from a plus to a minus etc
+
+        icon.classList.toggle('fa-plus');
+        icon.classList.toggle('fa-minus');
+
+        // Open the body when it is clicked
+
+        groupBody.classList.toggle('open');
+
+        // close the faq content when another is opened
+
+        const otherGroups = faqContainer.querySelectorAll('.faq-group');
+
+        otherGroups.forEach(function(otherGroup){
+            if (otherGroup !== group) {
+                const otherGroupBody = otherGroup.querySelector('.faq-group-body');
+                const otherIcon = otherGroup.querySelector('.faq-group-header i');
+
+                otherGroupBody.classList.remove('open');
+                otherIcon.classList.remove('fa-minus');
+                otherIcon.classList.add('fa-plus');
+
+            }
+        })
+        
+    })
+} );
